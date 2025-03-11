7bcb5ca84e956dedd093c96e19e000dd61e5f436
AlexDev07 <alekeymurcia@gmail.com>
2025-03-11 11:01:11 -0600

    Areglamos diseño

--- bears.js
+++ bears.js
@@ -1,33 +1,36 @@
-const type = "sfw"; // Cambia esto a "nsfw" si es necesario
-const category = "dance"; // Cambia esto a la categoría que prefieras
-const url = `https://api.waifu.pics/many/${type}/${category}`;
+function getWaifus() {
+  let type = document.getElementById("type").value;
+  let category = document.getElementById("category").value;
+  const url = `https://api.waifu.pics/many/${type}/${category}`;
 
-const requestData = { exclude: [] }; // Puedes agregar URLs que quieras excluir en este array
+  const requestData = { exclude: [] }; // Puedes agregar URLs que quieras excluir en este array
 
-fetch(url, {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify(requestData),
-})
-  .then(response => response.json())
-  .then(data => {
-    if (data.files && data.files.length > 0) {
-      const cardsContainer = document.getElementById("waifu-cards");
-      data.files.forEach(imageUrl => {
-        const card = document.createElement("div");
-        card.className = "card";
-        card.innerHTML = `
-          <img src="${imageUrl}" alt="${category} image">
-        `;
-        cardsContainer.appendChild(card);
-      });
-    } else {
-      document.getElementById("error").textContent = "No se encontraron imágenes.";
-    }
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(requestData),
   })
-  .catch(error => {
-    console.error("Error:", error);
-    document.getElementById("error").textContent = "Ocurrió un error al obtener las imágenes.";
-  });
+    .then(response => response.json())
+    .then(data => {
+      if (data.files && data.files.length > 0) {
+        const cardsContainer = document.getElementById("waifu-cards");
+        cardsContainer.innerHTML = ""; // Limpia los resultados anteriores
+        data.files.forEach(imageUrl => {
+          const card = document.createElement("div");
+          card.className = "card";
+          card.innerHTML = `
+            <img class="image-crop" src="${imageUrl}" alt="${category} image">
+          `;
+          cardsContainer.appendChild(card);
+        });
+      } else {
+        document.getElementById("error").textContent = "No se encontraron imágenes.";
+      }
+    })
+    .catch(error => {
+      console.error("Error:", error);
+      document.getElementById("error").textContent = "Ocurrió un error al obtener las imágenes.";
+    });
+}
